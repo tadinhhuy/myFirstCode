d9434dfe14ef5a2e3b7847a7d54517e35bfde430
Huy Ta <46718082+tadinhhuy@users.noreply.github.com>
2019-04-05 14:36:07 +0700

    Update AlgorithmBasic.js

--- AlgorithmBasic.js
+++ AlgorithmBasic.js
@@ -202,3 +202,17 @@ function frankenSplice(arr1,arr2,n){
   return newArr;
 }
 (frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2));
+
+
+
+function sumAll(arr) {
+  var max = Math.max(arr[0],arr[1]);
+  var min = Math.min(arr[0],arr[1]);
+  var temp = 0;
+  for(var i = min; i <= max; i++){
+    temp += i;
+  }
+  return temp;
+}
+sumAll([1, 4]);
+
