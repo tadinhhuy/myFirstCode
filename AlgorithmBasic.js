a0692f0fb20468ef9ee3bc610c0f2f759d58fb85
Huy Ta <46718082+tadinhhuy@users.noreply.github.com>
2019-04-11 16:53:14 +0700

    Update AlgorithmBasic.js

--- AlgorithmBasic.js
+++ AlgorithmBasic.js
@@ -286,3 +286,15 @@ function selectionSort(arr){
   }
   return arr;
 }
+
+//Insertion Sort:
+function insertionSort(arr){
+  for(let i = 0; i < arr.length; i++){
+    let temp = arr[i];
+    for(var j = i - 1;temp < arr[j]; j--){
+      arr[j+1] = arr[j];
+    }
+    arr[j+1] = temp;
+  }
+  return arr;
+}
