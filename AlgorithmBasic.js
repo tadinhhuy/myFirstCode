3e2e6fd1bc5c650df2e8a86624f5bf0e394e1758
Huy Ta <46718082+tadinhhuy@users.noreply.github.com>
2019-04-11 10:44:16 +0700

    Update AlgorithmBasic.js

--- AlgorithmBasic.js
+++ AlgorithmBasic.js
@@ -253,3 +253,18 @@ function getIndexToIns(arr,num){
 //   return arr.indexOf(num);
 }
 getIndexToIns([40, 60], 50);
+
+//Bubble Sort: 
+function bubbleSort(arr){
+  for(let i = 0; i < arr.length;i++){
+    for(let j = 0; j < arr.length-1;j++){
+      if(arr[j] > arr[j+1]){
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+bubbleSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
