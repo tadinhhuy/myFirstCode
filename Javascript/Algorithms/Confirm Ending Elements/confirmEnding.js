8b3d3b3efe91edeeb8090d6a5b28723d7c4cd448
Huy Ta <46718082+tadinhhuy@users.noreply.github.com>
2019-07-10 22:49:23 +0700

    Update confirmEnding.js

--- Javascript/Algorithms/Confirm Ending Elements/confirmEnding.js
+++ Javascript/Algorithms/Confirm Ending Elements/confirmEnding.js
@@ -1,4 +1,5 @@
 function confirmEnding(str, target){
-	return str.slice(str.length - target.length) === target; //return true or false?
+	return str.slice(str.length - target.length) === target;
+	//return true or false?
 }
 (confirmEnding("Bastian",n));
