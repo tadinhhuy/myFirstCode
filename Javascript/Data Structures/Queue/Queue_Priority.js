3f9839aacf67d0203169e19f775261a2d6f9e2ce
Huy Ta <46718082+tadinhhuy@users.noreply.github.com>
2019-07-13 00:25:48 +0700

    Create Queue_Priority.js

---
+++ Javascript/Data Structures/Queue/Queue_Priority.js
@@ -0,0 +1,40 @@
+function PriorityQueue () {
+    let collection = [];
+    this.printCollection = function() {
+      console.log(collection);
+    };
+    // Only change code below this line
+    this.enqueue = function(element) {
+      if(this.isEmpty()){
+        collection.push(element);
+      }else{
+        let added = false;
+        for(let i = 0; i < collection.length; i++){
+          if(element[1] < collection[i][1]){ //checking priority
+            collection.splice(i,0,element);
+            added = true;
+            break;
+          }
+        }
+        if(!added){
+          collection.push(element);
+        }
+      }
+    }
+    this.dequeue = function(){
+      let value = collection.shift();
+      return value[0];
+    }
+    this.size = function(){
+      return collection.length;
+    }
+    this.front = function(){
+      return collection[0];
+    }
+    this.isEmpty = function(){
+      return collection.length === 0;
+    }
+    // Only change code above this line
+}
+
+
