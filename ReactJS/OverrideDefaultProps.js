34b374d99f606c4ae83cfd7e41cfe4cee0541bc1
Huy Ta <46718082+tadinhhuy@users.noreply.github.com>
2019-07-17 22:03:19 +0700

    Update OverrideDefaultProps.js

--- ReactJS/OverrideDefaultProps.js
+++ ReactJS/OverrideDefaultProps.js
@@ -1,3 +1,4 @@
+```jsx
 const Items = (props) => {
   return( 
     <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
@@ -18,3 +19,4 @@ class ShoppingCart extends React.Component {
   
   }
 };
+```
