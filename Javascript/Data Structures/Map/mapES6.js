77dd0fe957c16c2a0c9032c04c37bd78c7646384
Huy Ta <46718082+tadinhhuy@users.noreply.github.com>
2019-07-21 00:29:26 +0700

    Create mapES6.js

---
+++ Javascript/Data Structures/Map/mapES6.js
@@ -0,0 +1,38 @@
+let Map = function() {
+	this.collection = {};
+	this.count = 0;
+	this.size = function() {
+		return this.count;
+	};
+	this.add = function(key, value) {
+		this.collection[key] = value;
+		this.count++;
+	};
+	this.has = function(key) {
+		return (key in this.collection);
+	};
+	this.get = function(key) {
+		return (key in this.collection) ? this.collection[key] : null;
+	};
+	this.remove = function(key) {
+		if (key in this.collection) {
+			delete this.collection[key];
+			this.count--;
+		}
+	};
+	this.values = function() {
+		let result = new Array();
+		for (let key of Object.keys(this.collection)) {
+			result.push(this.collection[key]);
+		};
+		return (result.length > 0) ? result : null;
+	};
+	this.clear = function() {
+		this.collection = {};
+		this.count = 0;
+	};
+};
+let myMap = new Map();
+myMap.add('Nick',2);
+myMap.add('finger', 10);
+console.log(myMap.count);
