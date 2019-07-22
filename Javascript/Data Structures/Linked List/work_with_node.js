20283a6fa593c8d7a3d4a60b270f41924fbb0d96
Huy Ta <46718082+tadinhhuy@users.noreply.github.com>
2019-07-22 17:25:56 +0700

    Create work_with_node.js

---
+++ Javascript/Data Structures/Linked List/work_with_node.js
@@ -0,0 +1,17 @@
+
+var Node = function(element){
+    this.element = element; 
+    this.next = null; 
+};
+
+var Kitten = new Node("Kitten");
+var Puppy = new Node("Puppy");
+var Cat = new Node("Cat");
+var Dog = new Node("Dog");
+
+Kitten.next = Puppy;
+Puppy.next = Cat;
+Cat.next = Dog;
+
+//for test
+console.log(Kitten.next);
