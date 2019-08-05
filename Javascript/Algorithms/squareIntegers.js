1ce366176b78987bf9fdf4ea6c30bf9db3990283
Huy Ta <46718082+tadinhhuy@users.noreply.github.com>
2019-08-05 23:13:53 +0700

    Create squareIntegers.js

---
+++ Javascript/Algorithms/squareIntegers.js
@@ -0,0 +1,13 @@
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+  "use strict";
+ 
+  const squaredIntegers = arr.filter( (num) => num > 0 && num % parseInt(num) === 0 ).map( (num) => Math.pow(num, 2) );
+
+  return squaredIntegers;
+  
+};
+
+// for test
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
