4c12ef1a042a9b289dfb03a26ffa1c33d5ff80ad
Huy Ta <46718082+tadinhhuy@users.noreply.github.com>
2019-08-09 17:49:28 +0700

    Update add_subtract.jsx

--- ReactJS/add_subtract.jsx
+++ ReactJS/add_subtract.jsx
@@ -1,35 +1,79 @@
-class MyComponent extends React.Component {
+import React, { Component } from 'react';
+
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       itemCount: 0
     };
 
-    this.addItem = this.addItem.bind(this);
-    this.removeItem = this.removeItem.bind(this);
-
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
   }
-  addItem() {
+  increment() {
     this.setState({
       itemCount: this.state.itemCount + 1
     });
   }
-  removeItem() {
+  decrement() {
     this.setState({
       itemCount: this.state.itemCount - 1
+    })
+  }
+  reset() {
+    this.setState({
+      itemCount: this.state.itemCount = 0
     })
   }
   render() {
     return (
       <div>
-
-        <button onClick={this.addItem}>Click Me</button>
-        <br/>
-        <button onClick={this.removeItem}>Remove Item</button>
-
+        <button onClick={this.increment}>Increment</button>
+        <br />
+        <button onClick={this.decrement}>Decrement</button>
+        <br />
+        <button onClick={this.reset}>Reset</button>
         <h1>Current Item Count: {this.state.itemCount}</h1>
-        
+        <MyComponent />
       </div>
     );
+  }
+};
+
+export default App;
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false
+    };
+
+    this.toggleVisibility = this.toggleVisibility.bind(this);
+
+  }
+
+  toggleVisibility() {
+    this.setState({
+      visibility: !this.state.visibility
+    })
+  }
+
+  render() {
+    if (this.state.visibility) {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+          <h1>Now you see me!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+        </div>
+      );
+    }
   }
 };
